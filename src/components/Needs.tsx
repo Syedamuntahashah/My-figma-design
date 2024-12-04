392971a6949d1dcfd213254f07632031567ff493
syedamuntahashah <syedashah32399@gmail.com>
2024-12-04 15:17:17 +0500

    This is my design create with tha help of figma templete

--- src/components/Needs.tsx
+++ src/components/Needs.tsx
@@ -13,7 +13,7 @@ export default function Needs() {
             <p className="font-sans font-normal text-[18px] leading-[30px] tracking-[-2%] w-[669px] h-[90px] mt-17">Customise the app with plugins custom themes and multiple text editors (Rich Text or Markdown). Or create your own scripts and plugins using the Extension API.</p>
             </div>
             <div className="flex items-center w-[171px] h-[63px] rounded-lg py-[20px] px-[40px] gap-[10px] bg-[#4F9CF9] mt-14">
-                <button className="font-sans font-medium text-[18px] leading-[23px] tracking-[-2%] text-[#FFFFFF]">Let's Go</button>
+                <button className="font-sans font-medium text-[18px] leading-[23px] tracking-[-2%] text-[#FFFFFF]">Let s Go</button>
                 <Image src="/Group 214.png" alt="" width={14} height={14}
                 className="rounded-[1px]"/>
             </div>
